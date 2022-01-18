26c599d9c8bbd971cf4e33b8539b40a7e5e8a9c9
diegoestradaXO <est.diego03@gmail.com>
2022-01-18 11:27:40 -0600

    minimal changes and readme

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.purple),
     title: 'Localizations Sample App',
     localizationsDelegates: [
       GlobalMaterialLocalizations.delegate,

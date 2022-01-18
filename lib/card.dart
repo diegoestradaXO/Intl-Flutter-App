11615b8d7e94c4724786dc56b5557e36d7650120
diegoestradaXO <est.diego03@gmail.com>
2022-01-18 12:56:14 -0600

    drawer implemented also with translations

---
+++ lib/card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatefulWidget {
+  const TaskCard({ Key? key }) : super(key: key);
+
+  @override
+  _TaskCardState createState() => _TaskCardState();
+}
+
+class _TaskCardState extends State<TaskCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}

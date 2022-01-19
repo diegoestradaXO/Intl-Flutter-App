a1654e6753969af8b2ed14985de0842e0f14aafe
diegoestradaXO <est.diego03@gmail.com>
2022-01-19 09:18:27 -0600

    card widget created

--- lib/card.dart
+++ lib/card.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TaskCard extends StatefulWidget {
-  const TaskCard({ Key? key }) : super(key: key);
+class TaskCard extends StatelessWidget {
+  final String? title;
+  TaskCard({this.title});
 
-  @override
-  _TaskCardState createState() => _TaskCardState();
-}
 
-class _TaskCardState extends State<TaskCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      margin: EdgeInsets.only(bottom: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Text(
+          title ?? "(Unnamed task)",
+          style: TextStyle(
+          fontSize: 22.0,
+          fontWeight: FontWeight.bold,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 10.0),
+          child: Text(
+          'Welcome to this app, try adding some cards or editing existing cards',
+          style: TextStyle(
+            fontSize: 16,
+            height: 1.5
+
+          ),
+        ),
+        ),
+        ],
+      ),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.white,
+       ),
+      padding: EdgeInsets.symmetric(
+        vertical: 32.0,
+        horizontal: 24.0, 
+      ),
     );
   }
 }
